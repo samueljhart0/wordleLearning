b714510c3c8820c934c946f49b75e470e45f087e
samueljhart0 <95101301+samueljhart0@users.noreply.github.com>
2022-03-04 14:44:58 -0500

    bug fixes, redesign of backend

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from game import Game
+from colorama import Fore, Back, init
+
+init(autoreset=True)
+
+# print("S L " + Fore.GREEN + "A " + Fore.BLACK + "T E ")
+
+# print([c for c in "SLATE"])
 
 game = Game()
 game.playGames()
