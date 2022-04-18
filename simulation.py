e5593f10b2d7f7c7b60b5552b9a62dcd1cb426b2
samueljhart0 <95101301+samueljhart0@users.noreply.github.com>
2022-04-17 20:54:31 -0400

    Add data vis

--- simulation.py
+++ simulation.py
@@ -1,22 +1,141 @@
-from game import Game
+import numpy as np
+from game import ANSWER_FILE, Game
 from tqdm import tqdm
+from data import WORD_FILE, Data
+import itertools as it
+import random
+import matplotlib.pyplot as plt
+from colorama import init
+from  matplotlib.colors import LinearSegmentedColormap
 
-def simulateAll():
-    with open("data/allowed_words.txt", "r") as f:
+
+def letterDistributions():
+    with open(WORD_FILE, 'r') as f:
         guessList = list(f.read().upper().split())
-    with open("data/answer_words.txt", "r") as f:
-        answerList = list(f.read().upper().split())
+    
+    letters = np.array(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+    engFreq = np.array([0.082, 0.015, 0.027, 0.047, 0.13, 0.022, 0.02, 0.062, 0.069, 0.0016, 0.0081, 0.04, 0.027,
+                        0.067, 0.078, 0.019, 0.0011, 0.059, 0.062, 0.096, 0.027, 0.0097, 0.024, 0.0015, 0.02, 0.00078])
+    engInd = np.argsort(engFreq)[::-1]
+    alphabet = dict(zip(letters, it.count()))
+    heatMap = np.zeros((26,5))
+    letterBuckets = np.zeros(26)
+    for word in guessList:
+        for i, l in enumerate(word):
+            heatMap[alphabet[l]][i] += 1
+            letterBuckets[alphabet[l]] += 1
+    byLetter = heatMap / heatMap.sum(1).reshape(-1,1)
+    byPosition = heatMap / heatMap.sum(0).reshape(1,-1)
+    letterFreq = letterBuckets / letterBuckets.sum()
+    wordleInd = np.argsort(letterFreq)[::-1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.bar(letters[engInd], engFreq[engInd], color='green')
+    plt.ylabel('Frequency')
+    fig.tight_layout()
+    plt.savefig("plots/engFreq", dpi=300, bbox_inches="tight")
+
+    cmap=LinearSegmentedColormap.from_list('bg',["gold", "green"], N=256)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(byLetter, cmap=cmap)
+    plt.colorbar(im)
+    ax.set_xticks(np.arange(5), labels=[1,2,3,4,5])
+    ax.set_yticks(np.arange(26), labels=letters)
+    plt.setp(ax.get_xticklabels(), ha="center")
+    ax.set_title("Letter Distributions Over Positions")
+    fig.tight_layout()
+    plt.savefig("plots/byLetter", dpi=300, bbox_inches="tight")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(byPosition, cmap=cmap)
+    plt.colorbar(im)
+    ax.set_xticks(np.arange(5), labels=[1,2,3,4,5])
+    ax.set_yticks(np.arange(26), labels=letters)
+    plt.setp(ax.get_xticklabels(), ha="center")
+    ax.set_title("Position Distributions Over Letters")
+    fig.tight_layout()
+    plt.savefig("plots/byPostion", dpi=300, bbox_inches="tight")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.bar(letters[wordleInd], letterFreq[wordleInd], color='green')
+    plt.ylabel('Frequency')
+    fig.tight_layout()
+    plt.savefig("plots/wordleFreq", dpi=300, bbox_inches="tight")
+    
+
+def simulateAll(guessList):
+    dataStore = dict()
+    dataStore["wordsToIndex"] = dict(zip(guessList, it.count()))
+    dataStore["avg"] = [0 for _ in range(len(guessList))]
+    dataStore["buckets"] = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for _ in range(len(guessList))]
+
+    with open(ANSWER_FILE, "r") as f:
+        answerList = random.sample(list(f.read().upper().split()), 250)
+    
     for guess in guessList:
-        print(f"Start word: {guess}")
+        guessBuckets = [0 for _ in range(10)]
+        print(f"Start Word:{guess}")
         numerator = 0
         denominator = 0
         avg = 1
         for i in tqdm(range(len(answerList)), ncols=100, leave=True):
-            # print(f"\nAvg # of guesses to answer:{avg}")
             answer = answerList[i]
             game = Game(answer)
             trial = game.simulate(guess)
+            guessBuckets[trial - 1] += 1
             numerator += trial
             denominator += 1
             avg = numerator / denominator
+        idx = dataStore["wordsToIndex"][guess]
+        dataStore["avg"][idx] = avg
+        dataStore["buckets"][idx] = guessBuckets
         print(f"Avg # of guesses to answer:{avg}")
+    return dataStore
+
+def buildPlots():
+    with open(ANSWER_FILE, "r") as f:
+        trueAnswerList = list(f.read().upper().split())
+    # Plot first step entropies
+    data = Data(answer=None, allowed=None, possible=trueAnswerList)
+    topTen = data.giveTop(10)
+    words, infos = [], []
+    for i in range(10):
+        word, info  = topTen[i][0], topTen[i][1]
+        words.append(word)
+        infos.append(info)
+    fig = plt.figure(figsize=(8,4))
+    ax = fig.add_subplot(111)
+    plt.bar(words, infos)
+    plt.xlabel('Start Word')
+    plt.ylabel('Expected bits of Information')
+    for i, v in enumerate(infos):
+        ax.text(i, v + 0.05, "%.2f" %v, ha="center")
+    plt.savefig("plots/entropiesFirstStep")
+    # Plot second step entropies
+    secondSteps = []
+    for word, stepOne in zip(words, infos):
+        patternProbs = data.getPatternProbs([word], data.possible).flatten()
+        for i in range(242):
+            patternProb = patternProbs[i]
+            thisData = data.copy()
+            thisData.processInput(word, i)
+            topGuess = thisData.giveTop(1)
+            stepOne += patternProb * topGuess[0][1]
+        secondSteps.append(stepOne)
+    fig = plt.figure(figsize=(8,4))
+    ax = fig.add_subplot(111)
+    plt.bar(words, secondSteps)
+    plt.xlabel('Start Word')
+    plt.ylabel('Expected bits after second guess')
+    for i, v in enumerate(secondSteps):
+        ax.text(i, v + 0.05, "%.2f" %v, ha="center")
+    plt.savefig("plots/entropiesSecondStep")
+    # Make selection of good words
+    selection = ["TARES", "SLATE", "SALET", "SOARE", "ARISE", "ADIEU", "CRANE", "TRACE", "CRATE"]
+
+    # Simulate these and plot their histograms and display their averages
--- util.py
+++ util.py
@@ -2,10 +2,9 @@
 Utility file for storing functions for project
 """
 
-from ast import For
 import math
 import random
-from colorama import init, Fore, Back
+# from colorama import init, Fore, Back
 
 def create_dict(list) -> dict[str, bool]:
     dict = {}
@@ -13,53 +12,53 @@ def create_dict(list) -> dict[str, bool]:
         dict[x] = True
     return dict
 
-def printAsKeyboard(dict) -> None:
-    firstRow  = "Q W E R T Y U I O P "
-    secondRow = " A S D F G H J K L  "
-    thirdRow  = "  Z X C V B N M     "
-    for f in firstRow:
-        if f == " ":
-            continue
-        else:
-            info = dict[f]
-            if info == "b":
-                firstRow = firstRow.replace(f"{f} ", Fore.BLACK + "  ")
-            elif info == "G":
-                firstRow = firstRow.replace(f"{f} ", Fore.GREEN + f"{f} ")
-            elif info == "Y":
-                firstRow = firstRow.replace(f"{f} ", Fore.YELLOW + f"{f} ")
-            elif info == "B":
-                firstRow = firstRow.replace(f"{f} ", Fore.BLACK + f"{f} ")
-    for s in secondRow:
-        if s == " ":
-            continue
-        else:
-            info = dict[s]
-            if info == "b":
-                secondRow = secondRow.replace(f"{s} ", Fore.BLACK + "  ")
-            elif info == "G":
-                secondRow = secondRow.replace(f"{s} ", Fore.GREEN + f"{s} ")
-            elif info == "Y":
-                secondRow = secondRow.replace(f"{s} ", Fore.YELLOW + f"{s} ")
-            elif info == "B":
-                secondRow = secondRow.replace(f"{s} ", Fore.BLACK + f"{s} ")
-    for t in thirdRow:
-        if t == " ":
-            continue
-        else:
-            info = dict[t]
-            if info == "b":
-                thirdRow = thirdRow.replace(f"{t} ", "  ")
-            elif info == "G":
-                thirdRow = thirdRow.replace(f"{t} ", Fore.GREEN + f"{t} ")
-            elif info == "Y":
-                thirdRow = thirdRow.replace(f"{t} ", Fore.YELLOW + f"{t} ")
-            elif info == "B":
-                thirdRow = thirdRow.replace(f"{t} ", Fore.BLACK + f"{t} ")     
-    print(f"{firstRow}\n{secondRow}\n{thirdRow}")
+# def printAsKeyboard(dict) -> None:
+#     firstRow  = "Q W E R T Y U I O P "
+#     secondRow = " A S D F G H J K L  "
+#     thirdRow  = "  Z X C V B N M     "
+#     for f in firstRow:
+#         if f == " ":
+#             continue
+#         else:
+#             info = dict[f]
+#             if info == "b":
+#                 firstRow = firstRow.replace(f"{f} ", Fore.BLACK + "  ")
+#             elif info == "G":
+#                 firstRow = firstRow.replace(f"{f} ", Fore.GREEN + f"{f} ")
+#             elif info == "Y":
+#                 firstRow = firstRow.replace(f"{f} ", Fore.YELLOW + f"{f} ")
+#             elif info == "B":
+#                 firstRow = firstRow.replace(f"{f} ", Fore.BLACK + f"{f} ")
+#     for s in secondRow:
+#         if s == " ":
+#             continue
+#         else:
+#             info = dict[s]
+#             if info == "b":
+#                 secondRow = secondRow.replace(f"{s} ", Fore.BLACK + "  ")
+#             elif info == "G":
+#                 secondRow = secondRow.replace(f"{s} ", Fore.GREEN + f"{s} ")
+#             elif info == "Y":
+#                 secondRow = secondRow.replace(f"{s} ", Fore.YELLOW + f"{s} ")
+#             elif info == "B":
+#                 secondRow = secondRow.replace(f"{s} ", Fore.BLACK + f"{s} ")
+#     for t in thirdRow:
+#         if t == " ":
+#             continue
+#         else:
+#             info = dict[t]
+#             if info == "b":
+#                 thirdRow = thirdRow.replace(f"{t} ", "  ")
+#             elif info == "G":
+#                 thirdRow = thirdRow.replace(f"{t} ", Fore.GREEN + f"{t} ")
+#             elif info == "Y":
+#                 thirdRow = thirdRow.replace(f"{t} ", Fore.YELLOW + f"{t} ")
+#             elif info == "B":
+#                 thirdRow = thirdRow.replace(f"{t} ", Fore.BLACK + f"{t} ")     
+#     print(f"{firstRow}\n{secondRow}\n{thirdRow}")
 
 def buildRandList(number):
-    with open("allowed_words.txt", "r") as f:
+    with open("data/allowed_words.txt", "r") as f:
         guessList = list(f.read().upper().split())
     randList = random.sample(guessList, k = number)
     return randList
@@ -99,6 +98,20 @@ def log(number):
         return math.log2(number)
     else:
         return 0
+
+def allowedPatterns(pattern, patterns):
+    return [modAlwaysLess(pattern, p) for p in patterns]
+
+def modAlwaysLess(pattern1, pattern2):
+    while pattern1 > 1 and pattern2 > 1:
+        rem1 = pattern1 % 3
+        rem2 = pattern2 % 3
+        if rem1 < rem2:
+            return False
+        pattern1 = pattern1 // 3
+        pattern2 = pattern2 // 3
+    return True
+
 
 if __name__ == '__main__':
     patterns = dict()
